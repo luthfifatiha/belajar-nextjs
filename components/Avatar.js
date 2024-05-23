bdff7bb278ea1eb33c2f0dee7128db64ba2db92e
luthfifatiha <luthfifatiha04@gmail.com>
2024-05-23 18:57:08 +0700

    Feat: Menyelesaikan page index

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,11 @@
+import Image from 'next/image';
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (
+    <div className='hidden xl:flex xl:max-w-none xl:justify-end xl:relative xl:top-1'>
+      <Image src={'/upi.png'} width={737} height={678} alt='' className='translate-z-0 w-full h-full'/>
+    </div>
+  )
 };
 
 export default Avatar;
