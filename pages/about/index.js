2cbe0661a76dce7928a2d2ae16ccb1002e25a16a
luthfifatiha <luthfifatiha04@gmail.com>
2024-06-02 09:13:06 +0700

    Feat: Menambahkan isi page about

--- pages/about/index.js
+++ pages/about/index.js
@@ -101,6 +101,7 @@ const About = () => {
   return (
     <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
       <Circles />
+
       {/* Avatar */}
       <motion.div
         variants={fadeIn("right", 0.2)}
@@ -111,9 +112,18 @@ const About = () => {
       >
         <Avatar />
       </motion.div>
+
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
-        <div className="flex-1 flex flex-col justify-center">text</div>
-        <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
+        <div className="flex-1 flex flex-col justify-center mb-8">
+          <h2 className="h2 mb-2">Natus Vincere</h2>
+          <p class="font-normal text-base max-w-xl lg:text-lg">
+            Hello, My Name's Muhammad Luthfi Fatiha, you can call me Upi or Ufi. My hobby is Coding and playing Badminton. I have a dream to be a programmer, hope i can make it happen.
+          </p>
+        </div>
+
+        {/* about */}
+        <div className="flex flex-col w-full xl:max-w-[48%] h-[200px]">
+          {/* title */}
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
             {aboutData.map((item, itemIndex) => {
               return (
@@ -130,16 +140,31 @@ const About = () => {
               );
             })}
           </div>
-          <div>
-          {aboutData[index].info.map((item, itemIndex) => {
-            return (
-              <div key={itemIndex}>
-                {/* title */}
-                <div>{item.title}</div>
-              </div>
-            );
-          })}
-        </div>
+
+          {/* point */}
+          <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
+            {aboutData[index].info.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className="flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
+                >
+                  {/* title */}
+                  <div className="font-light mb-2 md:mb-0">{item.title}</div>
+                  <div className="hidden md:flex">-</div>
+                  <div>{item.stage}</div>
+                  <div className="flex gap-x-4">
+                    {/* icons */}
+                    {item.icons?.map((icon, itemIndex) => {
+                      return (
+                        <div className="text-2xl text-white mb-3">{icon}</div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
